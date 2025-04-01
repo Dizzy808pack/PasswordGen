44bbd0f317ba682f326c3bb0bc30508339b95046
Dizzy808pack <marcusebanks12@gmail.com>
2025-03-31 23:33:50 -0400

    Version 0.2 of Password Generator
    Added a copy button, and a pop-up alert to notify user text was copied to clipboard

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -7,15 +7,36 @@ def GenPasswd():
     random_string = ''.join(random.choices(string.ascii_letters + string.digits, k=8))
     return random_string
 
-def click(): 
+#generateButton
+def GenClick(): 
+    CopyAlert()
     passString = GenPasswd()
     password_string.set(passString)
-    window.clipboard_append(passString) #adds password to clipboard
+    ClipboarbFunc()
+
+
+#CopyButton
+def CopyPasswdClick():
+        if (password_string.get() != ""):
+            
+            CopyAlert()
+            ClipboarbFunc()
+
+#adds password to clipboard
+def ClipboarbFunc():
+     window.clipboard_clear()
+     window.clipboard_append(password_string.get())
+
+#Copy Success Alert
+def CopyAlert():
+    alert_label = Label(bottom_frame, text= 'Copied to clipboard')
+    alert_label.pack()
+    alert_label.after(3500, alert_label.destroy)
 
 #Create Window
 fastPass = 0
 window = Tk()
-window.title('Password Generator v0.1')
+window.title('Password Generator v0.2')
 window.geometry('300x110')
 window.resizable(False, False)
 #Intialize Variables
@@ -23,17 +44,19 @@ password_string = StringVar()
 
 #Objects Area
 input_frame = Frame(master = window, pady= 10)
-button = Button(master = input_frame, text='Generate')
+generateButton = Button(master = input_frame, text='Generate')
 password_label = Label(master=input_frame, textvariable= password_string, font=('Calibri 20'))
+copyButton = Button(master = input_frame, text='🗍', font=('Calibri', 15, 'bold'), command=CopyPasswdClick)
 
-button.config(command=click, font=('Calibri', 15, 'bold'))
+generateButton.config(command=GenClick, font=('Calibri', 15, 'bold'))
+bottom_frame = Frame(window, relief='raised')
 
 #Packing Everything
-button.pack(side='left', padx =10)
+generateButton.pack(side='left', padx =10)
 password_label.pack(side = 'left')
-
+copyButton.pack(side = 'right')
 
 input_frame.pack(pady = 10)
-
+bottom_frame.pack(anchor=CENTER)
 #Run
 window.mainloop()
